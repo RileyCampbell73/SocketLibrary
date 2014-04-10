93f7e27138ab0c3e16c90e439c6083e2aaa59cbd
jhaig <jhaig@infotech.com>
2014-04-09 20:47:28 -0400

    merged both branches

--- C++Project 2/SocketLibrary/SocketLibrary.cpp
+++ C++Project 2/SocketLibrary/SocketLibrary.cpp
@@ -1,54 +1,5 @@
 #include "SocketLib.hpp"
 
-class MySocket{
-protected:
-	WSAData wsaData;
-	SOCKET hSocket;
-	bool isClient;
-	sockaddr_in service;
-public: 
-	MySocket(){//have this take in something to indicate whether its UDP or TCP. 
-				//ALSO maybe something to indicate whether its a client or server(maybe)
-	}
-
-	~MySocket(){
-
-	}
-
-};
-
-class TCPSocket : MySocket
-{
-public:
-	TCPSocket( bool client = false )
-	{
-		isClient = client;
-	}
-};
-	
-class UDPSocket : MySocket{
-public:
-
-	UDPSocket( bool client): MySocket(){
-		
-		isClient = client;
-	}
-
-	~UDPSocket(){
-		// terminate
-		closesocket(hSocket);
-		WSACleanup();
-	}
-
-	boolean OpenConnection(const char * , int );
-
-	void Sendmessage(std::string);
-
-	void Sendmessage(std::string, const char *, int);
-
-};
-
-
 boolean UDPSocket::OpenConnection(const char * address, int port){
 
 	int iResult = WSAStartup( MAKEWORD(2,2), &wsaData );
@@ -108,3 +59,47 @@ void UDPSocket::Sendmessage(std::string message, const char * address, int port)
 		);
 }
 	
+bool TCPSocket::OpenConnection( const char * address, int port )
+{
+	int iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+	if(iResult != NO_ERROR)
+	{
+		//cerr << "WSAStartup failed!" << endl;
+		return EXIT_FAILURE;
+	}
+
+	SOCKET hSocket = socket( AF_INET, SOCK_STREAM, IPPROTO_TCP );
+	if( hSocket == INVALID_SOCKET )
+	{
+		//cerr << "Error at socket(): " << WSAGetLastError << endl;
+		//exitCode = EXIT_FAILURE;
+		//goto cleanupWSA; // just jumps right to the cleanup: label
+		return EXIT_FAILURE;
+	}
+
+	service;
+	service.sin_family = AF_INET;
+	service.sin_addr.s_addr = inet_addr(address);
+	service.sin_port = htons(port);
+
+	if( !isClient )
+	{
+		if(bind(hSocket, (SOCKADDR*)&service, sizeof(service)) == SOCKET_ERROR) // bind prepares a machine for transmission (sits on the port and "owns" it for a while, in preparation for sending data).  Makes sure the port is open and that we can use it.
+		{
+			//cerr << "Bind() failed" << endl;
+			//goto cleanupSocket;
+			return false;
+		}
+
+		if(listen(hSocket,1)==SOCKET_ERROR) // sit on the socket and port and listen for an attempt to connect
+			// params:  first is socket, second is backlog
+				// backlog = how many attempts to connect can be held in the backlog, since we only have 1 we'll be attentive to only one and forget the rest
+					// other connections will FAIL when we've got one attempt already
+		{
+			//cerr << "Error listening on socket" << endl;
+			//goto cleanupSocket;
+			return false;
+		}
+	}
+	return true;
+}

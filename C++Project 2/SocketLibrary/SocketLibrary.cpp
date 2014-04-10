4bae5414260687ce61e43af78fdaa6ffb7ceba50
RileyCampbell73 <riley.campbell73@gmail.com>
2014-04-09 20:29:16 -0400

    MORE STUFF

--- C++Project 2/SocketLibrary/SocketLibrary.cpp
+++ C++Project 2/SocketLibrary/SocketLibrary.cpp
@@ -1,57 +1,23 @@
 #include "SocketLib.hpp"
 
-class Socket{
-public: 
+class MySocket{
+protected:
 	WSAData wsaData;
 	SOCKET hSocket;
 	bool isClient;
 	sockaddr_in service;
-
-	Socket(){//have this take in something to indicate whether its UDP or TCP. 
+public: 
+	MySocket(){//have this take in something to indicate whether its UDP or TCP. 
 				//ALSO maybe something to indicate whether its a client or server(maybe)
 	}
 
-	~Socket(){
-		// terminate
-		closesocket(hSocket);
-		WSACleanup();
+	~MySocket(){
+
 	}
 
-	boolean OpenConnection(const char * , int );
 };
 
-//FOR UDP SO FAR
-boolean Socket::OpenConnection(const char * address, int port){
-
-	int iResult = WSAStartup( MAKEWORD(2,2), &wsaData );
-	if(iResult!=0) {
-		//cerr<<"WSAStartup failed: " << iResult << endl;
-		//return EXIT_FAILURE;
-		return false;
-	}
-	else{
-		hSocket = socket( AF_INET, SOCK_DGRAM, 0 );
-		// Create the server address
-		sockaddr_in serverAddress = { 0 };
-		serverAddress.sin_family = AF_INET;
-		serverAddress.sin_port = htons(port);
-		serverAddress.sin_addr.s_addr	= inet_addr(address);
-			return true;
-	}
-
-	//THIS CODE IS FOR SERVER ONLY 
-	// wait for a message
-	//int res = bind(hSocket, (sockaddr*)&serverAddress, sizeof(serverAddress));
-	//if(res==SOCKET_ERROR) {
-	//	cout << "Error on bind\n";
-	//	res = WSAGetLastError();
-	//	cout << "Result: "<< res<< endl;
-	//	closesocket(hSocket);
-	//	return EXIT_FAILURE;
-	//}
-}
-
-class TCPSocket : Socket
+class TCPSocket : MySocket
 {
 public:
 	TCPSocket( bool client = false )
@@ -59,4 +25,86 @@ public:
 		isClient = client;
 	}
 };
+	
+class UDPSocket : MySocket{
+public:
+
+	UDPSocket( bool client): MySocket(){
+		
+		isClient = client;
+	}
+
+	~UDPSocket(){
+		// terminate
+		closesocket(hSocket);
+		WSACleanup();
+	}
+
+	boolean OpenConnection(const char * , int );
+
+	void Sendmessage(std::string);
+
+	void Sendmessage(std::string, const char *, int);
+
+};
+
+
+boolean UDPSocket::OpenConnection(const char * address, int port){
+
+	int iResult = WSAStartup( MAKEWORD(2,2), &wsaData );
+	if(iResult!=0) {
+		//cerr<<"WSAStartup failed: " << iResult << endl;
+		//return EXIT_FAILURE;
+		return false;
+	}
+		hSocket = socket( AF_INET, SOCK_DGRAM, 0 );
+		
+	
+
+	// Create the server address
+	//service = { 0 };
+	service.sin_family = AF_INET;
+	service.sin_port = htons(port);
+	service.sin_addr.s_addr	= inet_addr(address);
+
+	if (isClient == false){
+
+			//bind service to socket
+			int res = bind(hSocket, (sockaddr*)&service, sizeof(service));
+			if(res==SOCKET_ERROR) {
+			//	cout << "Error on bind\n";
+				res = WSAGetLastError();
+			//	cout << "Result: "<< res<< endl;
+				closesocket(hSocket);
+			//	return EXIT_FAILURE;
+				return false;
+			}
+	}	
+	return true;
+}
+
+void UDPSocket::Sendmessage(std::string message){
+
+	
+	sendto(hSocket, message.c_str(), message.size(), 0, 
+			(sockaddr*)&service, 
+			sizeof(service)
+			);
+
+}
+
+void UDPSocket::Sendmessage(std::string message, const char * address, int port){
+
+
+	//sockaddr_in clientAddress = { 0 };
+	//clientAddress.sin_family = AF_INET;
+	//clientAddress.sin_port = htons(port);
+
+	//clientAddress.sin_addr.s_addr= htonl(address);
+
+	sendto(hSocket, message.c_str(), message.size(), 0, 
+		(sockaddr*)&service, 
+		sizeof(service)
+		);
+}
 	
